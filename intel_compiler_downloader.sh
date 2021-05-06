3cfbde34f5311dafdd147c0dc2c39ee0af2824ae
Chivier Humber <chivier.humber@outlook.com>
2021-05-06 18:24:04 +0800

    Add Intel compiler downloader

---
+++ intel_compiler_downloader.sh
@@ -0,0 +1,2 @@
+wget https://registrationcenter-download.intel.com/akdlm/irc_nas/17769/l_BaseKit_p_2021.2.0.2883_offline.sh
+wget https://registrationcenter-download.intel.com/akdlm/irc_nas/17764/l_HPCKit_p_2021.2.0.2997_offline.sh
