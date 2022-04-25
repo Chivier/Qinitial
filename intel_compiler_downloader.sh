e8723b4099450490e6d75fc7696ac1a4d79d7c7b
Chivier Humber <chivier.humber@outlook.com>
2022-04-25 20:24:43 +0800

    udpate download part

--- intel_compiler_downloader.sh
+++
@@ -1,2 +0,0 @@
-wget https://registrationcenter-download.intel.com/akdlm/irc_nas/17769/l_BaseKit_p_2021.2.0.2883_offline.sh
-wget https://registrationcenter-download.intel.com/akdlm/irc_nas/17764/l_HPCKit_p_2021.2.0.2997_offline.sh
---
+++ qinitial.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# SSH prepare
+if [[ -e $HOME/.ssh/id_rsa && -e $HOME/.ssh/id_rsa.pub ]]; then
+    echo "Copy public key"
+    cat $HOME/.ssh/id_rsa.pub
+else
+    echo "Generating public key"
+    ssh-keygen
+    echo "Copy public key"
+    cat $HOME/.ssh/id_rsa.pub
+fi
+
+# Download ssh config file
+cp .ssh/config .ssh/config_bakup
+scp root@bastion.chivier.site:.ssh/config .ssh/config
+
+
+# GNU Compilers
+sudo apt install gcc-10 g++-10 gcc-9 g++-9 gcc-8 g++-8 gdb
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 8
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 9
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 9
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 10
+
+
+# Download from bastion
+if [[ ! -e $HOME/opt ]]; then
+    echo "Generating opt directory..."
+    mkdir -p $HOME/opt
+fi
+
+if [[ ! -e $HOME/opt/llvm ]]; then
+    echo "Downloading LLVM..."
+    scp -r bastion:share/tools/llvm $HOME/opt
+fi
+
+if [[ ! -e $HOME/opt/cmake ]]; then
+    echo "Downloading Cmake..."
+    scp -r bastion:share/tools/cmake $HOME/opt
+fi
+
+if [[ ! -e $HOME/opt/oneapi ]]; then
+    echo "Downloading oneapi..."
+    scp -r bastion:share/tools/oneapi $HOME/opt
+fi
+
+
+# Install LLVM
+
+# Install OneAPI
+
+# Install CMake
+
